0307cb2f22ce16c6cc8d7815565326a1849d23dc
Hellrungj <johnjameshellrung@gmail.com>
2019-11-06 22:23:42 -0600

    Update media static files

--- squatch_watch/settings.py
+++ squatch_watch/settings.py
@@ -141,12 +141,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'media/files/'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media/files/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
